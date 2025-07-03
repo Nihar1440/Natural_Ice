ab1d02311ea0fe2eb1ce7e15894320021101eae3
Nihar <niharh.brainerhub@gmail.com>
2025-07-03 18:47:49 +0530

    orders and return order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -17,7 +17,7 @@ export const createOrder = async (req, res) => {
   }
 };
 
-export const getOrder = async (req, res) => {
+export const getOrderById = async (req, res) => {
   try {
     const { id } = req.params;
     const order = await Order.findById(id);
@@ -29,6 +29,15 @@ export const getOrder = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getOrder =async(req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 export const updateOrder = async (req, res) => {
   try {
@@ -55,6 +64,7 @@ export const deleteOrder = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 export const returnOrder = async (req, res) => {
   try {
     const { id } = req.params;
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createOrder, getOrder, updateOrder, deleteOrder, returnOrder } from '../controllers/order.Controller.js';
+import { createOrder, getOrder, updateOrder, deleteOrder, returnOrder, getOrderById } from '../controllers/order.Controller.js';
 
 const router = express.Router();
 
 router.post('/orders', createOrder);
-router.get('/orders/:id', getOrder);
+router.get('/orders', getOrder);
+router.get('/orders/:id', getOrderById);
 router.put('/orders/:id', updateOrder);
 router.delete('/orders/:id', deleteOrder);
 router.post('/orders/:id/return', returnOrder);
 
-export default router; 
+export default router;
