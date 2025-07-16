13c8394dc5f3ef05dedf3a4ceb26ca00de01f06d
Ashik Parmar <ashikparmar5123@gmail.com>
2025-07-16 13:00:17 +0530

    added port

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'.env'
 })
+const port = process.env.PORT || 2000;
+
 connectDB().then(()=>{
-app.listen(2000,()=>{
-    console.log(`server is running`);
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
     
 })
 }).catch((err)=>{
