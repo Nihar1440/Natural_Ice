e07feffbaa6998990a5c30dada127ea8d7944ba8
Nihar Hota <niharh.brainerhub@gmail.com>
2025-07-28 20:04:51 +0530

    refund process completed

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createCheckoutSession ,storeOrderAfterPayment} from '../controllers/stripe.controller.js';
+import { createCheckoutSession ,initiateRefund,storeOrderAfterPayment} from '../controllers/stripe.controller.js';
 import { getPaymentDetails } from '../controllers/payment.controller.js';
+import { isAdmin } from '../middlewares/authmiddleware.js';
 const router = express.Router();
+
 router.post('/create-checkout-session', createCheckoutSession);
 router.post('/store-order', storeOrderAfterPayment);
+router.post('/initiate-refund',isAdmin, initiateRefund);
 
 router.get('/payment-details/:orderId', getPaymentDetails);
 
