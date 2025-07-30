299c9bd0a75cc6c9989e87dbdbc39fe676c63bb2
Nihar Hota <niharh.brainerhub@gmail.com>
2025-07-30 20:12:36 +0530

    payment controller added and fixed bugs

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createCheckoutSession ,initiateRefund,storeOrderAfterPayment} from '../controllers/stripe.controller.js';
-import { getPaymentDetails } from '../controllers/payment.controller.js';
+import { getAllPayments, getPaymentDetails, getPaymentDetailsByEmail, getUserPayments } from '../controllers/payment.controller.js';
 import { isAdmin } from '../middlewares/authmiddleware.js';
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post('/store-order', storeOrderAfterPayment);
 router.post('/initiate-refund',isAdmin, initiateRefund);
 
 router.get('/payment-details/:orderId', getPaymentDetails);
+router.get('/all-payments',isAdmin, getAllPayments);
+router.get('/payment-details-by-email/:email', getPaymentDetailsByEmail);
+router.get('/user-payments/:userId', getUserPayments);
 
 export default router;
