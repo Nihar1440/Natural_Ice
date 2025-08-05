6ed1e25153da2cb64c5b427e49f03e9d11eb188d
Nihar Hota <niharh.brainerhub@gmail.com>
2025-08-05 15:31:18 +0530

    cancel order refund flow implemented

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createCheckoutSession ,initiateRefund,storeOrderAfterPayment} from '../controllers/stripe.controller.js';
+import { createCheckoutSession ,initiateCancelledOrderRefund,initiateReturnRequestRefund, storeOrderAfterPayment} from '../controllers/stripe.controller.js';
 import { getAllPayments, getPaymentDetails, getPaymentDetailsByEmail, getUserPayments } from '../controllers/payment.controller.js';
 import { isAdmin } from '../middlewares/authmiddleware.js';
 const router = express.Router();
 
 router.post('/create-checkout-session', createCheckoutSession);
 router.post('/store-order', storeOrderAfterPayment);
-router.post('/initiate-refund',isAdmin, initiateRefund);
+
+router.post('/initiate-refund/returned-order/:returnOrderId',isAdmin, initiateReturnRequestRefund);
+router.post('/initiate-refund/cancelled-order/:cancelOrderId',isAdmin, initiateCancelledOrderRefund);
 
 router.get('/payment-details/:orderId', getPaymentDetails);
 router.get('/all-payments',isAdmin, getAllPayments);
